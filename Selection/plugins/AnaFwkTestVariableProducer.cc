39fee6c06f1e53f9c1cecd3e976da7d44dc4b75b
Wells Wulsin <wulsin.1@osu.edu>
2015-06-08 12:02:39 -0400

    Added calculation of jet relative pt difference

--- Selection/plugins/AnaFwkTestVariableProducer.cc
+++ Selection/plugins/AnaFwkTestVariableProducer.cc
@@ -11,6 +11,7 @@ AnaFwkTestVariableProducer::AddVariables (const edm::Event &event) {
 
   // Add all of the needed collections to objectsToGet_
   objectsToGet_.insert ("muons");
+  objectsToGet_.insert ("basicjets");
 
   // get all the needed collections from the event and put them into the "handles_" collection
   anatools::getRequiredCollections (objectsToGet_, collections_, handles_, event);
@@ -23,7 +24,18 @@ AnaFwkTestVariableProducer::AddVariables (const edm::Event &event) {
     addUserVar("muonPt", value, muon1);
   }
 
-}
+  // calculate relative pt difference
+  // There should be only 2 leading basicjets. 
+  if ((*handles_.basicjets).size() != 2) {
+    cout << "ERROR:  number of basic jets should be exactly 2!  " << endl;  
+  } else {
+    double pt1 = (*handles_.basicjets).at(0).pt();  
+    double pt2 = (*handles_.basicjets).at(1).pt();  
+    double relPtDiff = (pt1 - pt2) / (pt1 + pt2);  
+    addUserVar("basicjetRelPtDiff", relPtDiff, (*handles_.basicjets).at(0), (*handles_.basicjets).at(1));  
+  }
+
+}  
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(AnaFwkTestVariableProducer);
