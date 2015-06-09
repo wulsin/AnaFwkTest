41fa2f89877e163318c0b090bd63339be7cad53d
Andrew Hart <ahart@cern.ch>
2015-06-09 12:08:42 -0400

    First draft of producer for sqrt(y). Yet to be tested.

--- Selection/plugins/AnaFwkTestSubjetVariableProducer.cc
+++ Selection/plugins/AnaFwkTestSubjetVariableProducer.cc
@@ -1,3 +1,5 @@
+#include "TLorentzVector.h"
+
 #include "OSUT3Analysis/AnaTools/interface/CommonUtils.h"
 #include "AnaFwkTest/Selection/plugins/AnaFwkTestSubjetVariableProducer.h"
 #include "DataFormats/Math/interface/deltaR.h"
@@ -22,22 +24,41 @@ AnaFwkTestSubjetVariableProducer::AddVariables (const edm::Event &event) {
   //  cout << (*handles_.basicjets).at(0).subjets()).at(0).pt() << endl;
   //  cout << (*handles_.basicjets).at(0).getJetConstituents().at(0)->pt() << endl;
 
+  double sqrtY0 = 0.0, sqrtY1 = 0.0;
   if ((*handles_.basicjets).size() != 2) {
     cout << "ERROR:  number of basic jets should be exactly 2!  " << endl;
   } 
   else {
-    if ((*handles_.basicjets).at(0).getJetConstituents().size() >= 2) {
-      double subjet1pt = (*(*handles_.basicjets).at(0).getJetConstituents().at(0)).pt();
-      double subjet2pt = (*(*handles_.basicjets).at(0).getJetConstituents().at(1)).pt();
-      double mass = (*handles_.basicjets).at(0).mass();
-      //FIXME: start from here
-      //      double deltaR = 
-    }
-
-
-    //    (*eventvariables)["jet1nsubjets"] = (*handles_.basicjets).at(0).getJetConstituents().size();
-
+    if (handles_.basicjets->at (0).nConstituents () > 1)
+      {
+        pair<TLorentzVector, TLorentzVector> p;
+        p.first.SetPtEtaPhiE (handles_.basicjets->at (0).getJetConstituents ().at (0)->pt (),
+                              handles_.basicjets->at (0).getJetConstituents ().at (0)->eta (),
+                              handles_.basicjets->at (0).getJetConstituents ().at (0)->phi (),
+                              handles_.basicjets->at (0).getJetConstituents ().at (0)->energy ());
+        p.second.SetPtEtaPhiE (handles_.basicjets->at (0).getJetConstituents ().at (1)->pt (),
+                               handles_.basicjets->at (0).getJetConstituents ().at (1)->eta (),
+                               handles_.basicjets->at (0).getJetConstituents ().at (1)->phi (),
+                               handles_.basicjets->at (0).getJetConstituents ().at (1)->energy ());
+        sqrtY0 = min<double> (p.first.Pt (), p.second.Pt ()) * (p.first.DeltaR (p.second) / handles_.basicjets->at (0).mass ());
+      }
+    if (handles_.basicjets->at (1).nConstituents () > 1)
+      {
+        pair<TLorentzVector, TLorentzVector> p;
+        p.first.SetPtEtaPhiE (handles_.basicjets->at (1).getJetConstituents ().at (0)->pt (),
+                              handles_.basicjets->at (1).getJetConstituents ().at (0)->eta (),
+                              handles_.basicjets->at (1).getJetConstituents ().at (0)->phi (),
+                              handles_.basicjets->at (1).getJetConstituents ().at (0)->energy ());
+        p.second.SetPtEtaPhiE (handles_.basicjets->at (1).getJetConstituents ().at (1)->pt (),
+                               handles_.basicjets->at (1).getJetConstituents ().at (1)->eta (),
+                               handles_.basicjets->at (1).getJetConstituents ().at (1)->phi (),
+                               handles_.basicjets->at (1).getJetConstituents ().at (1)->energy ());
+        sqrtY0 = min<double> (p.first.Pt (), p.second.Pt ()) * (p.first.DeltaR (p.second) / handles_.basicjets->at (1).mass ());
+      }
   }
+
+  (*eventvariables)["minSqrtY"] = min<double> (sqrtY0, sqrtY1);
+  (*eventvariables)["maxSqrtY"] = max<double> (sqrtY0, sqrtY1);
 
 }  
 
